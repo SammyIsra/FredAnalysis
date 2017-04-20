f63f4cab3340a48d7cae7467a2a7214a84d27f98
Sammy Israwi <sammy.israwi@gmail.com>
2017-04-20 09:01:21 -0400

    Better colors for the bar

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -5,9 +5,18 @@ import '../style/Bar.css';
 class Bar extends React.Component {
 
     render(){
-        console.log(this.props);
+
+        const colorDict = {
+            anger: '#ff0000',
+            disgust: '#6600cc',
+            fear: '#33cc33',
+            joy: '#e6e600',
+            sadness: '#4da6ff'
+        }
+
         const barStyle = {
-            width: this.props.score*100 + '%'
+            width: this.props.score*100 + '%',
+            backgroundColor: colorDict[this.props.tone] || '#000099'
         }
         return (
             <div className={`Bar ${this.props.className}`} >
--- src/components/Tone.js
+++ src/components/Tone.js
@@ -10,10 +10,10 @@ function Tone({tone}){
                     {tone.tone_name}
                 </div>
                 <div className="twelve columns">
-                    {`${tone.score*100}%`}
+                    {`${(tone.score*100).toFixed(2)}%`}
                 </div>
                 <Bar className="twelve columns" 
-                    tone={tone.tone_name} 
+                    tone={tone.tone_id} 
                     score={tone.score} />
             </div>
         </td>
