5757062bb095ef9fd824b16213a2c0a5fa019511
Sammy Israwi <sammy.israwi@gmail.com>
2017-04-27 17:14:51 -0400

    Added the emoji loading bar

---
+++ src/components/EmojiBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class EmojiBar extends React.Component {
+
+    render(){
+
+        const {score, tone, className} = this.props;
+
+        console.log(this.props);
+
+        const efectiveScore = Math.round(100*score);
+
+        const emojiDict = {
+            joy: '😂',
+            anger: '😡',
+            disgust: '🤢',
+            fear: '😱',
+            sadness: '😢'
+        }
+
+        let loadedBar = "";
+        for(var i = 0 ; i < efectiveScore ; i++){
+            loadedBar += emojiDict[tone];
+            console.log(emojiDict[tone]);
+        }
+            
+
+        //remember, className is received from props
+        return (
+            <div className={`Bar ${className} ${tone}`} > 
+                <div className="bar-loaded">{loadedBar}</div>
+            </div>
+        );
+    }
+}
+
+export default EmojiBar;
