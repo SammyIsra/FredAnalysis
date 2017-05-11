3bea4e30f0864e3d587348e6d2b1e17879f8dfff
Sammy Israwi <sammy.israwi@gmail.com>
2017-05-11 19:26:10 -0400

    Styling of front page no longer is bad

--- src/components/ReportList.js
+++ src/components/ReportList.js
@@ -4,19 +4,34 @@ import {Link} from 'react-router-dom';
 
 import * as actions from '../actions';
 
+import '../style/ReportList.css';
+
 class ReportList extends React.Component {
     
     componentDidMount(){
         this.props.fetchAllShortSentiments();
     }
 
+    
+
     listOfReports(){
 
-        console.log(this.props)
+        function truncateText(text){
+            if(text.length <= 250)
+                return text;
+            else
+                return text.slice(0,247)+"...";
+        }
+
+        console.log(this.props.reports);
 
         if(this.props.reports.hasList){
             return this.props.reports.reportList.map(function(item){
-                return <li key={item._id}><Link to={`/report/${item._id}`}>{item.text}</Link></li>;
+                return (
+                    <li key={item._id} className="ReportListItem">
+                        <Link to={`/report/${item._id}`}>{truncateText(item.text)}</Link>
+                    </li>
+                );
             });
         } else {
             return <p>None yet!</p>;
